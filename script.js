74c161b15f6224df7aa75bbdcd2729e09381249b
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-17 14:35:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,91 @@
 // complete the js code
 class CustomPromise {
-  // write your code here
+  constructor(executor) {
+    this.state = "pending";
+    this.value = undefined;
+    this.reason = undefined;
+
+    this.onFulfilledCallbacks = [];
+    this.onRejectedCallbacks = [];
+
+    const resolve = (value) => {
+      if (this.state === "pending") {
+        this.state = "fulfilled";
+        this.value = value;
+        this.onFulfilledCallbacks.forEach(cb => cb());
+      }
+    };
+
+    const reject = (reason) => {
+      if (this.state === "pending") {
+        this.state = "rejected";
+        this.reason = reason;
+        this.onRejectedCallbacks.forEach(cb => cb());
+      }
+    };
+
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    return new CustomPromise((resolve, reject) => {
+      const handleFulfilled = () => {
+        setTimeout(() => {
+          try {
+            const result = onFulfilled ? onFulfilled(this.value) : this.value;
+            resolve(result);
+          } catch (err) {
+            reject(err);
+          }
+        }, 0);
+      };
+
+      const handleRejected = () => {
+        setTimeout(() => {
+          try {
+            if (onRejected) {
+              const result = onRejected(this.reason);
+              resolve(result);
+            } else {
+              reject(this.reason);
+            }
+          } catch (err) {
+            reject(err);
+          }
+        }, 0);
+      };
+
+      if (this.state === "fulfilled") {
+        handleFulfilled();
+      } else if (this.state === "rejected") {
+        handleRejected();
+      } else {
+        this.onFulfilledCallbacks.push(handleFulfilled);
+        this.onRejectedCallbacks.push(handleRejected);
+      }
+    });
+  }
+
+  catch(onRejected) {
+    return this.then(null, onRejected);
+  }
+
+  finally(onFinally) {
+    return this.then(
+      (value) => {
+        onFinally && onFinally();
+        return value;
+      },
+      (reason) => {
+        onFinally && onFinally();
+        throw reason;
+      }
+    );
+  }
 }
 
 window.CustomPromise = CustomPromise;
